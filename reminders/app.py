5b79abff3238dee89f398bf7924e4657c48c8f4d
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2021-12-19 11:36:42 -0800

    Added undone and marking date if expired.

--- reminders/app.py
+++ reminders/app.py
@@ -1,8 +1,10 @@
+import csv
+from datetime import datetime
 from os import write
+
 from flask import Flask
 from flask import render_template
 from flask import request
-import csv
 
 from werkzeug.utils import redirect
 
@@ -11,11 +13,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     reminders = []
+    expired = {}
+    today = datetime.today().date()
     with open('files/reminder.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             reminders.append(row)
-    return render_template('index.html', items = reminders)
+            item_date = datetime.strptime(row[0], "%Y-%m-%d").date()
+            if today > item_date:
+                expired[row[0]] = True
+
+    return render_template('index.html', items=reminders, expired_dates=expired)
 
 @app.route("/add", methods=['POST', 'GET'])
 def add():
@@ -25,7 +33,7 @@ def add():
 
         with open('files/reminder.csv', 'a') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow([date, reminder])
+            writer.writerow([date, reminder, 0])
         
         return redirect('/')
          
@@ -49,4 +57,37 @@ def delete(item_index):
         for row in reminders:
             writer.writerow(row)
     
+    return redirect("/")
+
+@app.route("/done/<int:item_index>")
+def done(item_index):
+    results = []
+    with open('files/reminder.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            results.append(row)
+    
+    results[item_index][2] = 1
+
+    with open('files/reminder.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in results:
+            writer.writerow(row)
+
+    return redirect("/")
+
+@app.route("/undone/<int:item_index>")
+def undone(item_index):
+    results = []
+    with open('files/reminder.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            results.append(row)
+
+    results[item_index][2] = 0
+    with open('files/reminder.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in results:
+            writer.writerow(row)
+
     return redirect("/")
