0772cf03f2a12b3effa8e525976ac40f2bb96ae1
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2021-12-19 00:43:03 -0800

    Styles, added delete feature

--- reminders/app.py
+++ reminders/app.py
@@ -1,3 +1,4 @@
+from os import write
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -29,3 +30,23 @@ def add():
         return redirect('/')
          
     return render_template('add.html')
+
+@app.route("/delete/<int:item_index>")
+def delete(item_index):
+    # load the contents of the csv file first
+    reminders = []
+    with open('files/reminder.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            reminders.append(row)
+
+    # remove the nth row
+    reminders.pop(item_index)
+
+    # save the file again from the reminders list
+    with open('files/reminder.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in reminders:
+            writer.writerow(row)
+    
+    return redirect("/")
