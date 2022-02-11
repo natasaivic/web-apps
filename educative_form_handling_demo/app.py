4548b069139b1554ca56459bb1778d6df7741344
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-02-11 13:52:08 -0800

    Rendering Flask-WTF forms in templates with error handling

--- educative_form_handling_demo/app.py
+++ educative_form_handling_demo/app.py
@@ -18,6 +18,32 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+
+    # if form.is_submitted():
+    #     print("Submitted.")
+    
+    # if form.validate():
+    #     print("Valid.")
+
+    # if form.validate_on_submit():
+    #    print("Submitted and Valid.")
+
+    if form.validate_on_submit():
+        for u_email, u_password in users.items():
+            if u_email == form.email.data and u_password == form.password.data:
+                return render_template("login.html", message ="Successfully Logged In")
+        return render_template("login.html", form = form, message ="Incorrect Email or Password")
+    
+
+    # if form.validate_on_submit():
+    #     print("Submitted and Valid.")
+    #     print("Email:", form.email.data)
+    #     print("Password:", form.password.data)
+    # elif form.errors:
+    #     print(form.errors.items())
+    #     print(form.email.errors)
+    #     print(form.password.errors)
+
     # if request.method == "POST":
     #     email = request.form["email"]
     #     password = request.form["password"]
